e999b363d2245152cad208aa6bd39655f4b9f657
xuwei-k <6b656e6a69@gmail.com>
2018-01-22 18:27:38 +0900

    more test

--- jvm/src/test/scala/scalapb_json/NameUtilsJVMTest.scala
+++ jvm/src/test/scala/scalapb_json/NameUtilsJVMTest.scala
@@ -5,14 +5,24 @@ import com.google.common.base.CaseFormat
 
 object NameUtilsJVMTest extends Scalaprops {
 
-  val `camelCaseToSnakeCase compatible guava` = Property.forAllG(Gen.asciiString) { s =>
+  private[this] val strGen: Gen[String] = Gen.genString(
+    Gen.oneOf(
+      Gen.asciiChar,
+      Gen.elements(
+        '\u00E0', '\u00E1', '\u00E2', '\u00E3', '\u00E4', '\u00E5', '\u00E6', '\u00C0', '\u00C1',
+        '\u00C2', '\u00C3', '\u00C4', '\u00C5', '\u00C6'
+      )
+    )
+  )
+
+  val `camelCaseToSnakeCase compatible guava` = Property.forAllG(strGen) { s =>
     val x = NameUtils.camelCaseToSnakeCase(s)
     val y = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, s)
     utest.assert(x == y)
     true
   }
 
-  val `lowerSnakeCaseToCamelCase compatible guava` = Property.forAllG(Gen.asciiString) { s =>
+  val `lowerSnakeCaseToCamelCase compatible guava` = Property.forAllG(strGen) { s =>
     val x = NameUtils.lowerSnakeCaseToCamelCase(s)
     val y = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, s)
     utest.assert(x == y)
